b763937eb6ac5981c4bd7c9f42ae7aae892d8c1c
Flavio Grandin <flavio.grandin@gmail.com>
2019-07-28 11:27:53 +0200

    cors header added

--- api.py
+++ api.py
@@ -13,6 +13,7 @@ logger = logging.getLogger()
 
 
 def handler(event, context):
+    response = {"headers": {"Access-Control-Allow-Origin": "*"}}
     try:
         params = json.loads(event['body'])
         objs = params['objs']
@@ -25,8 +26,10 @@ def handler(event, context):
 
         data = json.loads(table.to_pandas().to_json(orient=orient))
 
-        response = {"statusCode": 200, "body": json.dumps({"data": data})}
+        response["statusCode"] = 200
+        response["body"] = json.dumps({"data": data})
     except Exception as e:
         logger.error(str(e))
-        response = {"statusCode": 500, "body": json.dumps({"error": str(e)})}
+        response["statusCode"] = 500
+        response["body"] = json.dumps({"error": str(e)})
     return response
