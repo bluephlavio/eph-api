8203e7e8ad5e05e77d484aa87add2678364cbe87
Flavio Grandin <flavio.grandin@gmail.com>
2019-08-20 13:38:47 +0200

    meta added to response

--- api.py
+++ api.py
@@ -13,7 +13,7 @@ logger = logging.getLogger()
 
 
 def handler(event, context):
-    response = {"headers": {"Access-Control-Allow-Origin": "*"}}
+    response = {'headers': {'Access-Control-Allow-Origin': '*'}}
     try:
         params = json.loads(event['body'])
         objs = params['objs']
@@ -26,10 +26,10 @@ def handler(event, context):
 
         data = json.loads(table.to_pandas().to_json(orient=orient))
 
-        response["statusCode"] = 200
-        response["body"] = json.dumps({"data": data})
+        response['statusCode'] = 200
+        response['body'] = json.dumps({'meta': { 'objs': objs }, 'data': data})
     except Exception as e:
         logger.error(str(e))
-        response["statusCode"] = 500
-        response["body"] = json.dumps({"error": str(e)})
+        response['statusCode'] = 500
+        response['body'] = json.dumps({'error': str(e)})
     return response
